f9c7f3d30f62091fdaf997f35e0e68aad328383a
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-12-19 01:12:43 +0200

    Update to work with 2024 and all previous years

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # SteamReplayShowHours
+
 A script that shows how many hours you played this year in Steam Replay
 
 ## How to use:
+
 1. Go to the [Steam Replay page](https://store.steampowered.com/replay/)
 2. Open developer tools (F12 on Chrome and Firefox) and press on the "Console" tab on top
 3. Copy the script from **[HERE](https://raw.githubusercontent.com/Wilzzu/SteamReplayShowHours/main/index.js)** and run it on the console
 
 Also available on [Grease Fork!](https://greasyfork.org/en/scripts/457192-steam-replay-total-hours)
 
-![Example](https://i.imgur.com/R7kfM68.jpeg)
+![Example](https://i.imgur.com/KF3oc9R.png)
+![Example2](https://i.imgur.com/m7807HR.png)
--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 javascript: (function () {
+	const year = window.location.href.split("/").pop();
 	let data = JSON.parse(
 		application_config.dataset[
-			"yearinreview_" + JSON.parse(application_config.dataset.userinfo).accountid + "_2022"
+			`yearinreview_${JSON.parse(application_config.dataset.userinfo).accountid}_${year}`
 		]
 	).playtime_stats;
 
-	document.querySelector(".yirlanding_SectionTitle_AmQ9d").innerHTML =
-		"You spent " +
-		Math.round(data.total_stats.total_playtime_seconds / 3600) +
-		" hours playing this year!<br/><br/>" +
-		document.querySelector(".yirlanding_SectionTitle_AmQ9d").innerHTML;
+	const processedTitle = new WeakSet();
+	const processedMostPlayed = new WeakSet();
+	const processedOtherGames = new WeakSet();
 
-	document.querySelectorAll(".yirlanding_GridItem_2VmQC").forEach((e) => {
-		let appID = e
-			.querySelector(".yirlanding_BackgroundImage_2UVYU")
-			.style.backgroundImage.split("apps/")
-			.pop()
-			.split("/")[0];
-
-		if (!appID.includes("url")) {
-			let mainDiv = e.querySelector(".yirlanding_SummaryBlockExtrasCtn_1fY-T");
-			let statDiv = mainDiv.childNodes[0].cloneNode(true);
-			statDiv.childNodes[0].innerHTML = Math.round(
-				data.games.find((game) => game.appid == appID).stats.total_playtime_seconds / 3600
-			);
-			statDiv.childNodes[1].innerHTML = "Hours Played";
-			mainDiv.append(statDiv);
+	function updateTitle() {
+		const element = document.querySelector(".AmQ9dRhZqhOyU0Vgt3nWR");
+		if (element && !processedTitle.has(element)) {
+			processedTitle.add(element);
+			element.innerHTML =
+				"You spent " +
+				Math.round(data.total_stats.total_playtime_seconds / 3600) +
+				" hours playing games this year!<br/><br/>" +
+				element.innerHTML;
 		}
-	});
+	}
+
+	function updateMostPlayed() {
+		document.querySelectorAll("._2AXFQ4F1EgcZAVJgYC6_KQ").forEach((e) => {
+			if (processedMostPlayed.has(e)) return;
+			processedMostPlayed.add(e);
+
+			let appID = getAppID(e, "._2UVYU3krJstwNrAYNcRAtq");
+			addHoursPlayed(e, "._1fY-Tu9TH4Rv4r_U5vPalT", appID);
+		});
+	}
+
+	function updateOtherGames() {
+		document.querySelectorAll("._1o-OaJLHoJcCZBa1I48gl1").forEach((e) => {
+			if (processedOtherGames.has(e)) return;
+			processedOtherGames.add(e);
+
+			let appID = getAppID(e, "._3yp7l6ya6Tr2G_hZK0yrXz");
+			addHoursPlayed(e, "._3nXdDUZyLDuZxhkbe3WpO8", appID);
+		});
+	}
+
+	function addHoursPlayed(el, classname, id) {
+		if (id.includes("url")) return;
+		let mainDiv = el.querySelector(classname);
+		let statDiv = mainDiv.childNodes[0].cloneNode(true);
+		statDiv.childNodes[0].innerHTML = Math.round(
+			data.games.find((game) => game.appid == id).stats.total_playtime_seconds / 3600
+		);
+		statDiv.childNodes[1].innerHTML = "Hours Played";
+		mainDiv.append(statDiv);
+	}
+
+	function getAppID(el, classname) {
+		return el.querySelector(classname).style.backgroundImage.split("apps/").pop().split("/")[0];
+	}
+
+	function observeElements(callback) {
+		callback();
+
+		const observer = new MutationObserver((mutations) => {
+			let shouldRun = false;
+			for (const mutation of mutations) {
+				if (mutation.addedNodes.length > 0) {
+					shouldRun = true;
+					break;
+				}
+			}
+			if (shouldRun) callback();
+		});
+
+		observer.observe(document.body, { childList: true, subtree: true });
+	}
+
+	observeElements(updateTitle);
+	observeElements(updateMostPlayed);
+	observeElements(updateOtherGames);
 })();
